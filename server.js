ef1ef452ef4d548d140180e1e0d58d73e4c2c62f
beta21s <66484965+beta21s@users.noreply.github.com>
2021-09-01 20:21:01 +0700

    .

--- server.js
+++ server.js
@@ -6,53 +6,38 @@ var options = {
   cert: cert
 };
 
+
 const express = require("express");
 const app = express();
+
+let broadcaster;
+const port = 443;
+
 const server = require("https").Server(options, app);
-const { v4: uuidv4 } = require("uuid");
+
 const io = require("socket.io")(server);
+app.use(express.static(__dirname + "/public"));
 
-
-const { ExpressPeerServer } = require("peer");
-const peerServer = ExpressPeerServer(server, {
-  debug: true,
-});
-
-app.set("view engine", "ejs");
-app.use(express.static("public"));
-app.use("/peerjs", peerServer);
-
-app.get("/share", (req, res) => {
-  res.render("share");
-});
-
-app.get("/view", (req, res) => {
-  res.render("view");
-});
-
-
-// app.get("/", (req, rsp) => {
-//   rsp.redirect(`/${uuidv4()}`);
-// });
-
-// app.get("/:room", (req, res) => {
-//   res.render("room", { roomId: req.params.room });
-// });
-
-// io.on("connection", (socket) => {
-//   socket.on("room", (roomId, userId) => {
-//     socket.join(roomId);
-//     socket.to(roomId).broadcast.emit("newView", userId);
-//   });
-// });
-
-io.on('connection', function (socket) {
-  console.log('IO Connect: ' + socket.id);
-  socket.use(async (packet, next) => {
-      let topic = packet[0];
-      let data = packet[1];
-      io.sockets.emit(topic, data);
+io.sockets.on("error", e => console.log(e));
+io.sockets.on("connection", socket => {
+  socket.on("broadcaster", () => {
+    broadcaster = socket.id;
+    socket.broadcast.emit("broadcaster");
+  });
+  socket.on("watcher", () => {
+    socket.to(broadcaster).emit("watcher", socket.id);
+  });
+  socket.on("offer", (id, message) => {
+    socket.to(id).emit("offer", socket.id, message);
+  });
+  socket.on("answer", (id, message) => {
+    socket.to(id).emit("answer", socket.id, message);
+  });
+  socket.on("candidate", (id, message) => {
+    socket.to(id).emit("candidate", socket.id, message);
+  });
+  socket.on("disconnect", () => {
+    socket.to(broadcaster).emit("disconnectPeer", socket.id);
   });
 });
-
-server.listen(443, () => console.log('Server running in port ' + 443));
+server.listen(port, () => console.log(`Server is running on port ${port}`));
